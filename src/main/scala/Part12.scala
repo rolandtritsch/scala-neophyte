ec586c3fef7ef43c89b755d30a0b725dde5c4544
Roland Tritsch <roland@gonito.com>
2017-07-26 13:31:27 +0100

    Part12: Final commit. Working.

--- src/main/scala/Part12.scala
+++ src/main/scala/Part12.scala
@@ -1,23 +1,39 @@
 package neophyte.part12
 
-object Part12 {
-  def main(args: Array[String]): Unit = {
-    require(args.size == 0, s"Usage: ${Part12.getClass.getName.split('$').head}")
+import scala.concurrent.duration.{Duration, TimeUnit}
 
-    val xs = (1 to 10).map(_.toDouble).toVector
-    println(s"Mean: ${Statistics.mean(xs)}")
-    println(s"Median: ${Statistics.median(xs)}")
-    println(s"Quartiles: ${Statistics.quartiles(xs)}")
-    println(s"Iqr: ${Statistics.iqr(xs)}")
+// Note: https://stackoverflow.com/questions/25468583/why-cant-scala-find-my-typeclass-instance-defined-implicitly-in-the-companion-o
+object Math {
+  import annotation.implicitNotFound
+  @implicitNotFound("No member of type class NumberLike in scope for ${T}")
+  trait NumberLike[T] {
+    def plus(x: T, y: T): T
+    def minus(x: T, y: T): T
+    def devide(x: T, y: Int): T
+  }
 
-    println(s"Mean2: ${Statistics.mean2[Double](xs)}")
-    assert(Statistics.mean(xs) == Statistics.mean2(xs))
-    println(s"Median2: ${Statistics.median2(xs)}")
-    assert(Statistics.median(xs) == Statistics.median2(xs))
-    println(s"Quartiles2: ${Statistics.quartiles2(xs)}")
-    assert(Statistics.quartiles(xs) == Statistics.quartiles2(xs))
-    println(s"Iqr2: ${Statistics.iqr2(xs)}")
-    assert(Statistics.iqr(xs) == Statistics.iqr2(xs))
+  object NumberImplicits {
+    implicit object NumberLikeInt extends NumberLike[Int] {
+      override def plus(x: Int, y: Int): Int = x + y
+      override def minus(x: Int, y: Int): Int = x - y
+      override def devide(x: Int, y: Int): Int = x / y
+    }
+
+    implicit object NumberLikeDouble extends NumberLike[Double] {
+      override def plus(x: Double, y: Double): Double = x + y
+      override def minus(x: Double, y: Double): Double = x - y
+      override def devide(x: Double, y: Int): Double = x / y
+    }
+  }
+
+  object JodaImplicits {
+    import org.joda.time.Duration
+
+    implicit object NumberLikeDuration extends NumberLike[Duration] {
+      override def plus(x: Duration, y: Duration): Duration = x.plus(y)
+      override def minus(x: Duration, y: Duration): Duration = x.minus(y)
+      override def devide(x: Duration, y: Int): Duration = Duration.millis(x.getMillis/y)
+    }
   }
 }
 
@@ -47,36 +63,35 @@ object Statistics {
   }
 }
 
-object Math {
-  //import annotation.implicitNotFound
-  //@implicitNotFound("No member of type class NumberLike in scope for ${T}")
-  trait NumberLike[T] {
-    def plus(x: T, y: T): T
-    def minus(x: T, y: T): T
-    def devide(x: T, y: Int): T
-  }
+object Part12 {
+  def main(args: Array[String]): Unit = {
+    require(args.size == 0, s"Usage: ${Part12.getClass.getName.split('$').head}")
 
-  object NumberImplicits {
-    implicit object NumberLikeInt extends NumberLike[Int] {
-      override def plus(x: Int, y: Int): Int = x + y
-      override def minus(x: Int, y: Int): Int = x - y
-      override def devide(x: Int, y: Int): Int = x / y
-    }
+    val xs = (1 to 10).map(_.toDouble).toVector
+    println(s"Mean: ${Statistics.mean(xs)}")
+    println(s"Median: ${Statistics.median(xs)}")
+    println(s"Quartiles: ${Statistics.quartiles(xs)}")
+    println(s"Iqr: ${Statistics.iqr(xs)}")
 
-    implicit object NumberLikeDouble extends NumberLike[Double] {
-      override def plus(x: Double, y: Double): Double = x + y
-      override def minus(x: Double, y: Double): Double = x - y
-      override def devide(x: Double, y: Int): Double = x / y
-    }
-  }
+    import Math.NumberImplicits._
 
-  object JodaImplicits {
+    println(s"Mean2: ${Statistics.mean2(xs)}")
+    assert(Statistics.mean(xs) == Statistics.mean2(xs))
+    println(s"Median2: ${Statistics.median2(xs)}")
+    assert(Statistics.median(xs) == Statistics.median2(xs))
+    println(s"Quartiles2: ${Statistics.quartiles2(xs)}")
+    assert(Statistics.quartiles(xs) == Statistics.quartiles2(xs))
+    println(s"Iqr2: ${Statistics.iqr2(xs)}")
+    assert(Statistics.iqr(xs) == Statistics.iqr2(xs))
+
     import org.joda.time.Duration
+    import Math.JodaImplicits._
 
-    implicit object NumberLikeDuration extends NumberLike[Duration] {
-      override def plus(x: Duration, y: Duration): Duration = x.plus(y)
-      override def minus(x: Duration, y: Duration): Duration = x.minus(y)
-      override def devide(x: Duration, y: Int): Duration = Duration.millis(x.getMillis/y)
-    }
+    val ds = (1 to 10).map(Duration.standardSeconds(_)).toVector
+
+    println(s"Mean3: ${Statistics.mean2(ds)}")
+    println(s"Median3: ${Statistics.median2(ds)}")
+    println(s"Quartiles3: ${Statistics.quartiles2(ds)}")
+    println(s"Iqr3: ${Statistics.iqr2(ds)}")
   }
 }
